264f4732760859fcb4cd31c9de4ea00f59f04290
braddock-bresnahan <braddock.bresnahan@gmail.com>
2023-10-11 13:40:19 -0500

    Added text to all EItemType enums

--- app/src/main/java/com/example/mystylist/enums/EItemType.java
+++ app/src/main/java/com/example/mystylist/enums/EItemType.java
@@ -2,39 +2,39 @@ package com.example.mystylist.enums;
 
 @SuppressWarnings("unused")
 public enum EItemType {
-    T_SHIRT(""),
-    SLEEVELESS_SHIRT(""),
-    POLO(""),
-    LONG_SLEEVE_SHIRT(""),
-    BUTTON_DOWN_SHIRT(""),
-    BLOUSE(""),
-    PANTS(""),
-    SHORTS(""),
-    JEANS(""),
-    LONG_SKIRT(""),
-    SHORT_SKIRT(""),
-    SUIT_JACKET(""),
-    JACKET(""),
-    COAT(""),
-    WINDBREAKER(""),
-    SWEATER(""),
-    HOODIE(""),
-    DRESS(""),
-    SUNDRESS(""),
-    SPORTS_BRA(""),
-    SHORT_SOCKS(""),
-    LONG_SOCKS(""),
-    LEGGINGS(""),
-    SNEAKERS(""),
-    LOAFERS(""),
-    DRESS_SHOES(""),
-    HEELS(""),
-    HIGH_HEELS(""),
-    SANDALS("");
+    T_SHIRT("T-shirt"),
+    SLEEVELESS_SHIRT("Sleeveless Shirt"),
+    POLO("Polo"),
+    LONG_SLEEVE_SHIRT("Long Sleeve Shirt"),
+    BUTTON_DOWN_SHIRT("Button Down Shirt"),
+    BLOUSE("Blouse"),
+    PANTS("Pants"),
+    SHORTS("Shorts"),
+    JEANS("Jeans"),
+    LONG_SKIRT("Long Skirt"),
+    SHORT_SKIRT("Short Skirt"),
+    SUIT_JACKET("Suit Jacket"),
+    JACKET("Jacket"),
+    COAT("Coat"),
+    WINDBREAKER("Windbreaker"),
+    SWEATER("Sweater"),
+    HOODIE("Hoodie"),
+    DRESS("Dress"),
+    SUNDRESS("Sundress"),
+    SPORTS_BRA("Sports Bra"),
+    SHORT_SOCKS("Short Socks"),
+    LONG_SOCKS("Long Socks"),
+    LEGGINGS("Leggings"),
+    SNEAKERS("Sneakers"),
+    LOAFERS("Loafers"),
+    DRESS_SHOES("Dress Shoes"),
+    HEELS("Heels"),
+    HIGH_HEELS("High Heels"),
+    SANDALS("Sandals");
 
     public final String text;
 
-    private EItemType(String text) {
+    EItemType(String text) {
         this.text = text;
     }
 }
