7ba1e3d7f82d5bb2a1b6ba532ae0a225827cf637
braddock-bresnahan <braddock.bresnahan@gmail.com>
2023-10-26 18:37:11 -0500

    Created Outfit and ETag structures

---
+++ app/src/main/java/com/example/mystylist/structures/Outfit.java
@@ -0,0 +1,26 @@
+package com.example.mystylist.structures;
+
+import com.example.mystylist.enums.ETag;
+
+public class Outfit {
+    private final String outfitName;
+    private final String outfitDesc;
+    private final Item[] items;
+    private final long tagFlags;  // Binary mask
+
+    public Outfit(String outfitName, String outfitDesc, Item[] items, long tagFlags) {
+        this.outfitName = outfitName;
+        this.outfitDesc = outfitDesc;
+        this.items = items;
+        this.tagFlags = tagFlags;
+    }
+
+    public String getOutfitName() { return outfitName; }
+    public String getOutfitDesc() { return outfitDesc; }
+    public Item[] getItems() { return items; }
+    public long getTagFlags() { return tagFlags; }
+
+    public ETag[] getTags() {
+        return ETag.flagsToTags(tagFlags);
+    }
+}
