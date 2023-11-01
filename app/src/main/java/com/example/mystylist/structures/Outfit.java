df72299f67ba808d577217041bae03a07d9901a7
braddock-bresnahan <braddock.bresnahan@gmail.com>
2023-10-31 21:39:36 -0500

    Added documentation for the Outfit class.

--- app/src/main/java/com/example/mystylist/structures/Outfit.java
+++ app/src/main/java/com/example/mystylist/structures/Outfit.java
@@ -3,17 +3,44 @@ package com.example.mystylist.structures;
 import com.example.mystylist.enums.ETag;
 
 public class Outfit {
+    /**
+     * The name of the outfit.
+     */
     private final String outfitName;
+    /**
+     * The description of the outfit.
+     */
     private final String outfitDesc;
+    /**
+     * The items of the outfit.
+     */
     private final Item[] items;
+    /**
+     * The tags of the outfit as a bit mask.
+     */
     private final long tagFlags;  // Binary mask
 
+    /**
+     * Constructor.
+     * @param outfitName the name of the outfit.
+     * @param outfitDesc the description of the outfit.
+     * @param items the items of the outfit.
+     * @param tagFlags the tags of the outfit as a bit mask.
+     */
     public Outfit(String outfitName, String outfitDesc, Item[] items, long tagFlags) {
         this.outfitName = outfitName;
         this.outfitDesc = outfitDesc;
         this.items = items;
         this.tagFlags = tagFlags;
     }
+
+    /**
+     * Constructor.
+     * @param outfitName the name of the outfit.
+     * @param outfitDesc the description of the outfit.
+     * @param items the items of the outfit.
+     * @param tags the tags of the outfit as an array of ETags.
+     */
     public Outfit(String outfitName, String outfitDesc, Item[] items, ETag[] tags) {
         this.outfitName = outfitName;
         this.outfitDesc = outfitDesc;
@@ -21,19 +48,52 @@ public class Outfit {
         this.tagFlags = ETag.tagsToMask(tags);
     }
 
+    /**
+     * Returns the name of this outfit.
+     * @return a String of this outfit's name.
+     */
     public String getOutfitName() { return outfitName; }
+
+    /**
+     * Returns the description of this outfit.
+     * @return a String of this outfit's description.
+     */
     public String getOutfitDesc() { return outfitDesc; }
+
+    /**
+     * Returns the items in this outfit
+     * @return an Item array of this outfit's items.
+     */
     public Item[] getItems() { return items; }
+
+    /**
+     * Returns this outfit's tags as a bit mask.
+     * @return a bit mask representing this outfit's outfit tags.
+     */
     public long getTagFlags() { return tagFlags; }
 
+    /**
+     * Returns this outfit's tags as an ETag array.
+     * @return an array of ETags representing this outfit's tags.
+     */
     public ETag[] getTags() {
         return ETag.maskToTags(tagFlags);
     }
 
+    /**
+     * Returns whether this outfit satisfies the given mask.
+     * @param filterMask the mask to check.
+     * @return true, if this outfit satisfies the mask; else false.
+     */
     public boolean tagsSatisfyFilter(long filterMask) {
         return (tagFlags & filterMask) == filterMask;
     }
 
+    /**
+     * Returns whether the given item is in this outfit.
+     * @param item the item to check.
+     * @return true, if the item is in the outfit; else false.
+     */
     public boolean contains(Item item) {
         for (Item check : items) {
             if (item == check)
